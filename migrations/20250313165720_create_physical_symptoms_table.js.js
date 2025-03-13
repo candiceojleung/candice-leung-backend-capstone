29206a9c8229f399d030f157849467b356b0979e
candiceojleung <candice.leung@live.ca>
2025-03-13 13:02:20 -0400

    create migration files

---
+++ migrations/20250313165720_create_physical_symptoms_table.js.js
@@ -0,0 +1,10 @@
+export function up(knex) {
+    return knex.schema.createTable("physical_symptoms", (table) => {
+      table.increments("id").primary();
+      table.string("name").notNullable().unique().comment('Stored in singular lowercase');
+    });
+  }
+  
+  export function down(knex) {
+    return knex.schema.dropTable("physical_symptoms");
+  }
